08f39ad53ff6c455dbb26b4e09c7d40366a02eda
terminalmax <nishanthrao9@gmail.com>
2021-05-07 00:18:38 +0530

    Added setTime command

--- cogs/usertime.py
+++ cogs/usertime.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 import pytz
-
+import asyncio
 
 import discord
 from discord.ext import commands
@@ -14,7 +14,7 @@ class UsersTime(commands.Cog):
     
     @commands.has_role("Admin")
     @commands.command()
-    async def getTime(self, ctx):
+    async def getTime(self, ctx, user_name=None):
         with open('Data/timezones.json', 'r') as read_file:
             zones = json.load(read_file)
             embedVar = discord.Embed(title='Time')
@@ -26,12 +26,38 @@ class UsersTime(commands.Cog):
                 except AttributeError:
                     continue
             await ctx.send(embed=embedVar)
-            
+    
+    @commands.has_role("Admin")
+    async def setMyTime(self, ctx, selected_zone=None):
+
+        if selected_zone == None:
+            ctx.author.send(f"{pytz.all_timezones}")
+            ctx.author.send("Use '$setMyTime number_from_list' to set your timezone!")
+        else:
+            with open("Data/timezones.json", 'r+') as file:
+                file.seek(0);
+                zones = json.load(file)
+                zones[ctx.author.id] = pytz.all_timezones[selected_zone]
+            #FINISH THIS PART
 
     @commands.has_role("Admin")
     @commands.command()
-    async def setMyTime(self, ctx):
-        pass
+    async def quicksched(self, ctx, type):
+
+        def check_message(m):
+            if m.author.id == ctx.author.id and m.channel == discord.DMChannel():
+                return True
+
+        try:
+            await ctx.author.send("Give me the title(50 words):")
+            title = await self.client.wait_for("message", check= check_message, timeout=20)
+            await ctx.author.send("Give me the description(200 words):")
+            description = await self.client.wait_for("message", check=check_message, timeout=60)
+
+            self.client.guild[0].get_channel
+
+        except asyncio.TimeoutError:
+            await ctx.author.send("Too Slow ! ! ! !")
 
 def setup(client):
     client.add_cog(UsersTime(client))
