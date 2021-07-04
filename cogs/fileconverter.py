8d201a638ea545491771c63eab9bddcf39e39881
Nishanth Rao <nishanthrao9@gmail.com>
2021-07-04 10:56:14 +0530

    added status channel

--- cogs/fileconverter.py
+++ cogs/fileconverter.py
@@ -5,5 +5,9 @@ class FileConverter(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 
+	@commands.Command()
+	async def chess(self, ctx):
+		
+
 def setup(client):
 	client.add_cog(FileConverter(client))
