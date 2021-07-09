ba68dd8bf1d7ba92260f73334189975014fd3682
Nishanth Rao <nishanthrao9@gmail.com>
2021-07-09 12:15:18 +0530

    added sequential option

--- cogs/periodicmessages.py
+++ cogs/periodicmessages.py
@@ -10,24 +10,50 @@ class PeriodicMessages(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.message_channel = None
+        self.count = 0
 
     # Commands
     @commands.command()
-    async def startPeriodicMessages(self, ctx, arg=None):
+    async def strpm(self, ctx,arg=None):
         if arg == None:
             self.message_channel = ctx.channel
         else:
             self.message_channel = GENERAL_CHANNEL_ID
             
-        self.periodicMessage.start()
+        self.periodicRandomMessage.start()
     
     @commands.command()
-    async def stopPeriodicMessages(self, ctx):
+    async def stpm(self, ctx, arg=None):
+        self.message_channel = ctx.channel
+        self.periodicMessage.start()
+
+    @commands.command()
+    async def stoppm(self, ctx):
         self.periodicMessage.stop()
+
+    @commands.command()
+    async def stopprm(self, ctx):
+        self.periodicRandomMessage.stop()
 
     # Tasks
     @tasks.loop(hours=8.0)
     async def periodicMessage(self):
+        with open('Data/periodiclinks.json', 'r') as file:
+
+            data = json.load(file)
+
+            if self.count >= len(data['messages']):
+                return
+
+            message = data['messages'][self.count]
+            self.count = self.count + 1
+
+            print(message)
+            await self.message_channel.send('@everyone ' + message)
+
+
+    @tasks.loop(hours=8.0)
+    async def periodicRandomMessage(self):
         with open('Data/periodiclinks.json', 'r') as file:
             data = json.load(file)
             
